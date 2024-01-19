6f142d8b69527a1900369c907d3ef3dc23eccf71
Corey Munn <michael.munn@secunetics.com>
2024-01-19 09:37:47 -0500

    created fns for editing and deleting

--- src/api/index.js
+++ src/api/index.js
@@ -8,3 +8,11 @@ export const getTodos = async () => {
 export const createTodo = async (newTodo) => {
   await axios.post("http://localhost:5000/api/todo", newTodo);
 };
+
+export const editTodo = async (todo) => {
+  await axios.put(`http://localhost:5000/api/todo/${todo.id}`);
+};
+
+export const deleteTodo = async (todo) => {
+  await axios.delete(`http://localhost:5000/api/todo/${todo.id}`);
+};
