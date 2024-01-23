656889aa4e00786d7bc4798b1f2375a6def3bddf
Corey Munn <michael.munn@secunetics.com>
2024-01-23 13:46:10 -0500

    utilize baseUrl var in api routes

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,25 @@
 import axios from "axios";
 
+const baseUrl = "localhost";
+
 export const getTodos = async () => {
-  const res = axios.get("http://localhost:5000/api/todo");
+  const res = axios.get(`http://${baseUrl}:5000/api/todo`);
   return res;
 };
 
 export const createTodo = async (newTodo) => {
-  await axios.post("http://localhost:5000/api/todo", newTodo);
+  await axios.post(`http://${baseUrl}:5000/api/todo`, newTodo);
 };
 
 export const editTodo = async (todo) => {
-  await axios.put(`http://localhost:5000/api/todo/${todo.id}`, todo);
+  await axios.put(`http://${baseUrl}:5000/api/todo/${todo.id}`, todo);
 };
 
 export const deleteTodo = async (id) => {
-  await axios.delete(`http://localhost:5000/api/todo/${id}`);
+  await axios.delete(`http://${baseUrl}:5000/api/todo/${id}`);
 };
 
 export const reorderTodo = async (args) => {
   const { id, direction } = args;
-  await axios.put(`http://localhost:5000/api/todo/${id}/${direction}`, {});
+  await axios.put(`http://${baseUrl}:5000/api/todo/${id}/${direction}`, {});
 };
